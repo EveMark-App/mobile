dfb8a412113355e0f84b21da61923579628399c0
NoorMbarek <noormbarek1@gmail.com>
2023-01-13 20:56:05 +0100

    new Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,88 @@
-import React  from 'react';
-import { View, Image ,TextInput, TouchableOpacity,Text, StyleSheet  } from 'react-native';
+import React from "react";
+import {
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TouchableHighlight,
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 export default function Header() {
-    return (
+  return (
+    <View style={styles.container}>
+      <View style={styles.logo}>
+        <Image
+          source={require("../../../assets/logo.png")}
+          style={styles.logoImage}
+        />
+        <Text style={styles.logoText}>EveMark</Text>
+      </View>
 
-        <View style={{ flexDirection: 'row' ,}}>
-            <Image
-                source={require('../../../assets/logo.png')}
-                style={{ position: 'absolute', marginTop:'9%',marginLeft:'6%', width: 20, height: 15 }} />
-            <Text style={{position: 'absolute', marginTop:'10%',marginLeft:'38%' ,  width: 100, height: 100, fontSize: 20, color: '#0094D2', fontWeight: 'bold' }}> EveMark </Text>
-            <Image
-                source={require('../../../assets/Notifications.png')}
-                style={{ position:'absolute', top: 40, left: 320, width: 20, height: 20 }} />
-            <Image
-                source={require('../../../assets/Save.png')}
-                style={{ position: 'absolute', top: 40, left: 350, width: 20, height: 20 }} />
+      <View style={styles.void} />
 
-        </View>
-    );
+      <View style={styles.iconsContainer}>
+        <TouchableOpacity style={styles.icon}>
+          <Ionicons
+            name="notifications"
+            size={Dimensions.get("window").width * 0.06}
+            color="black"
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.icon}>
+          <Ionicons
+            name="bookmark"
+            size={Dimensions.get("window").width * 0.06}
+            color="black"
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-evenly",
+    flexDirection: "row",
+    marginTop: 40,
+    marginHorizontal: 20,
+    alignItems: "center",
+    height: Dimensions.get("window").width * 0.12,
+  },
+  logo: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    flex: 0.5,
+  },
+  logoImage: {
+    width: Dimensions.get("window").width * 0.12,
+    height: Dimensions.get("window").width * 0.12,
+  },
+  logoText: {
+    fontSize: Dimensions.get("window").width * 0.07,
+    fontWeight: "bold",
+    color: "black",
+  },
+  iconsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    flex: 0.3,
+  },
+  icon: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgray",
+    borderRadius: 10,
+    height: Dimensions.get("window").width * 0.1,
+    width: Dimensions.get("window").width * 0.1,
+  },
+  void: {
+    flex: 0.2,
+  },
+});
